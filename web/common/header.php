89894d56915071880a71e6b05c5abad02a27e67c
Cara311 <bat17018@byui.edu>
2020-04-27 22:02:28 -0600

    nav test

--- web/common/header.php
+++ web/common/header.php
@@ -10,7 +10,38 @@
 </head>
 <body>
     <header role="banner">
+    <div id="logoarea">
+<img id="logo-main" src="../images/CodeOtterLogo.png" alt="Code Otter Logo">
+</div>
+
+<nav id="navbar-primary" class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Assignments <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Assignment 1</a></li>
+            <li><a href="#">Assignment 2</a></li>
+            <li><a href="#">Assignment 3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Other</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <nav>
+        
         <?php include 'nav.php'; ?>
         <?php include $_SERVER['DOCUMENT_ROOT'].'nav.php'; ?>
     </nav>
