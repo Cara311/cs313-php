d918cc49ee55121968c0bbf69b21295415efc75c
Cara311 <bat17018@byui.edu>
2020-04-27 22:47:27 -0600

    footer

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,6 +1,11 @@
 </main>
 
 <footer>
+    <hr>
+    <p>&copy; CODE OTTER, All rights reserved.<br>
+        All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<br>
+        Last Updated: <?php echo date("l jS \of F Y"); ?>
+    </p>
 </footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
