6b3a8aaae1bf4df4e22a69016b2933a2c2592cd3
Cara311 <bat17018@byui.edu>
2020-04-30 21:59:23 -0600

    fix

--- web/views/home.php
+++ web/views/home.php
@@ -6,7 +6,11 @@
     
     <div class="col-sm-9 text-left center"> 
       <h1>Code Otter</h1>
-      <p>Welcome to Code Otter. This website is a portfolio of my school coding projects</p>
+      <p>Welcome to Code Otter. This website is a portfolio of my school coding projects.</p>
+      <hr>
+      <p>Otters are playful and are able to use the things they find around them to achieve their goals (like using a rock to break a mussel shell.
+        My coding is similar. It is about having fun while searching around me to find the best tools to achieve my goals.
+      </p>
     </div>
     <div class="col-sm-3 sidenav">
       <div class="well">
--- web/views/otters.php
+++ web/views/otters.php
@@ -4,7 +4,13 @@
     <div class="row content">
     <div class="col-sm-9 text-left center"> 
       <h1>Otters</h1>
-      <p>Coming Soon...</p>
+      <p>Here are a few fun otter facts:
+          <ul>
+              <li>There are 13 species of otters</li>
+              <li>They store food in the loose skin in their armpits</li>
+              <li>River otters can hold their breath for 8 minutes.</li>
+          </ul>
+      </p>
       <div>
     </div>
      
