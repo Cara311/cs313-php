03170b4ec66c9bfc04d4f8e089ac8c6a23ac6fef
Cara311 <bat17018@byui.edu>
2020-05-21 16:12:00 -0600

    styles

--- web/views/ideaDetails.php
+++ web/views/ideaDetails.php
@@ -24,7 +24,7 @@ foreach ($idea as $row)
     echo "<img class='card-img-top' src='../images/{$row[image_name]}' alt='{$row[gift_name]}' style='width:100%'>";
     echo "<div class='card-body'>";
     echo "<h4 class='card-title'>" . $row['gift_name'] . "</h4>";
-    echo "<p class='card-text'>" . $row['decription'] . "</p>";
+    echo "<p class='card-text'>" . $row['description'] . "</p>";
     echo "<p class='card-text price'>" . $row['price'] . "</p>";
     echo "</div>";
 
