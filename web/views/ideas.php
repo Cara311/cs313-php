2518247e9b80330b2e9942625448eac8c0eccaa5
Cara311 <bat17018@byui.edu>
2020-05-21 22:47:57 -0600

    style test

--- web/views/ideas.php
+++ web/views/ideas.php
@@ -21,16 +21,20 @@ $gifts = searchQuery($searchText, $db);
 <div class="container">
   <div class="row">
     <div class="col-sm-4"></div>
-    <div class="col-sm-4">
-    <ul>
-      <?php  
-      foreach ($gifts as $row)
-      {
-      echo "<li  class='idealist'><a href='ideaDetails.php?id={$row['id']}' <strong>" . $row['gift_name'] . '</strong></a></li>';
-      }
-      ?>
-      <li><a href="gift.php" class="btn btn-info bottombtn">Return to Search</a> </li>
-    </ul>
+    <div class="col-sm-4" id="ideaslist">
+      <div class='card'>
+        <div class='card-body'>
+          <ul>
+            <?php  
+              foreach ($gifts as $row)
+              {
+               echo "<li class='idealist card-text'><a href='ideaDetails.php?id={$row['id']}' <strong>" . $row['gift_name'] . '</strong></a></li>';
+              }
+            ?>
+          </ul>
+        </div>
+        <a href="gift.php" class="btn btn-info bottombtn">Return to Search</a> 
+      </div>  
       
     </div>
     <div class="col-sm-4"></div>
