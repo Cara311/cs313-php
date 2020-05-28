1af0facf164b7635193d28177a1f507b782ac39b
Cara311 <bat17018@byui.edu>
2020-05-27 23:49:14 -0600

    logged in test

--- web/views/list.php
+++ web/views/list.php
@@ -1,9 +1,20 @@
 <?php include '../common/header.php'; ?>
 
-<main>
-    <h1>Welcome to Your Account <?php $clientFirstname ?></h1>
 
-    
+<main>
+
+<?php if(isset($_SESSION['loggedin'])){ ?>
+            
+            <div class="welcome"> Welcome <?php echo $_SESSION['clientData']['clientFirstname'] ?></div>
+
+              <a href='/acme/accounts/index.php?action=logout' title='Click to logout'>Log Out</a><img src='/acme/images/site/account.gif' alt='red folder'>
+             <?php } else { ?>
+              <a href='../accounts/index.php?action=login' title='Click to register or login'>My Account</a> 
+           <?php }
+           ?>
+   
+
+
 
 </main>
 <?php include '../common/footer.php'; ?>
