251029a8d0eb87fbb6c031148bc760d322ba0f84
Cara311 <bat17018@byui.edu>
2020-05-27 23:38:16 -0600

    test

--- web/views/login.php
+++ web/views/login.php
@@ -22,22 +22,31 @@ require_once '../library/functions.php';
     <hr>
 
     <div class="container">
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="../accounts/index.php" method="POST">
+                        <input type="hidden" name="action" value="login_user">
 
-        <form action="../accounts/index.php" method="POST">
-            <input type="hidden" name="action" value="login_user">
-            <div class="form-group">
-                <label for="clientEmail">Email:*</label><br>
-                <input type="email" name="clientEmail" title="something@email.com" id="clientEmail" value="<?php echo (isset($clientEmail) ? $clientEmail : ' ');  ?>" required><br>
-            </div>
-            <div class="form-group">    
-                <label for="clientPassword">Password:*</label><br>
-                <span class="password">Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span><br> 
-                <input type="password" name="clientPassword" id="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required><br>
-            </div>
-            
-            <input type="submit" name="submit" id="logbttn"  class="button" value="Login">
+                        <div class="form-group">
+                            <label for="clientEmail">Email:*</label><br>
+                            <input type="email" name="clientEmail" title="something@email.com" id="clientEmail" value="<?php echo (isset($clientEmail) ? $clientEmail : ' ');  ?>" required><br>
+                        </div>
+                        <div class="form-group">    
+                            <label for="clientPassword">Password:*</label><br>
+                            <span class="password">Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span><br> 
+                            <input type="password" name="clientPassword" id="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required><br>
+                        </div>
+                         <input type="submit" name="submit" id="logbttn"  class="button" value="Login">
   
-        </form>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>    
    </div>
 
 </main>
