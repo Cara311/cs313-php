f5bd0b252257397f7bb5a9471d193dbcfbb5b2c3
Cara311 <bat17018@byui.edu>
2020-05-28 00:17:54 -0600

    hidden buttons

--- web/views/ideaDetails.php
+++ web/views/ideaDetails.php
@@ -39,10 +39,12 @@ foreach ($idea as $row)
     echo "<p class='card-text'>" . $row['description'] . "</p>";
     echo "<p class='card-text price'>" . $row['price'] . "</p>";
     //Only have these button show up if someone has signed up for an account.
+    if(isset($_SESSION['loggedin'])){
     echo " <input type='submit' name='add_to_list' class='btn btn-info' id='{$row[id]}' value='Save to Idea List'><br>";
     echo "<br>";
     echo "<a href='#' class='btn btn-info'>View Idea List</a>";
     echo "</div>";
+    }
     echo "<a href='gift.php' class='btn btn-info'>Go Back to Idea Search</a>";
     echo "</div>";
    // echo "</form>"
