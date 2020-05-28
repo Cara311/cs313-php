dc56007d0886c9db3b11e021d796ff26c5220963
Cara311 <bat17018@byui.edu>
2020-05-28 00:12:13 -0600

    fix

--- web/views/list.php
+++ web/views/list.php
@@ -4,9 +4,9 @@
 
 <?php if(isset($_SESSION['loggedin'])){ ?>
             
-            <div class="welcome"> Welcome <?php echo $_SESSION['clientData']['clientFirstname'] ?></div>
+            <div class="welcome"> Welcome <?php echo $_SESSION['clientData']['clientfirstname'] ?></div>
 
-              <a href='/acme/accounts/index.php?action=logout' title='Click to logout'>Log Out</a><img src='/acme/images/site/account.gif' alt='red folder'>
+              <a href='../accounts/index.php?action=logout' title='Click to logout'>Log Out</a>
              <?php } else { ?>
               <a href='../accounts/index.php?action=login' title='Click to register or login'>My Account</a> 
            <?php }
